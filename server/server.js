97ae32914cfcb43253b59d720aa41f90dc17c507
code-BSOD <mishkat2008@gmail.com>
2023-05-17 03:15:26 +0200

    modified server js with ex1 code n ex2 api added

--- server/server.js
+++ server/server.js
@@ -33,6 +33,7 @@ app.get('/', function homepage(req, res) {
  */
 
 app.get('/api', (req, res) => {
+  // FIXME: Document all your api endpoints below as a simple hardcoded JSON object.
   res.json({
     message: 'Welcome to my app api!',
     documentationUrl: '', //leave this also blank for the first exercise
@@ -41,26 +42,26 @@ app.get('/api', (req, res) => {
       {method: 'GET', path: '/api', description: 'Describes all available endpoints'},
       {method: 'GET', path: '/api/profile', description: 'Data about me'},
       {method: 'GET', path: '/api/books/', description: 'Get All books information'},
-      {method: 'POST', path: '/api/books/', description: 'Insert a new book information'},
-      {method: 'PUT', path: '/api/books/', description: 'Update a book information, based on id'},
-      {method: 'DELETE', path: '/api/books/', description: 'Delete a book information, based on id'},
-      // TODO: Write other API end-points description here like above
+      // FIXME: Write other API end-points description here like above
+      {method: 'POST', path: '/api/books', description: 'Add a book information into database'},
+      {method: 'PUT', path: '/api/books/:id', description: 'Update a book information based upon the specified ID'},
+      {method: 'DELETE', path: '/api/books/:id', description: 'Delete a book based upon the specified ID'}
     ]
   })
 });
-
+// FIXME:  Fill the values
 app.get('/api/profile', (req, res) => {
   res.json({
-    'name': 'John',
-    'homeCountry': 'Winterfell',
-    'degreeProgram': 'Night\'s Watch',//informatics or CSE.. etc
-    'email': 'john@got.com',
+    'name': 'Geralt of Rivia',
+    'homeCountry': 'Westeros',
+    'degreeProgram': 'DEA',//informatics or CSE.. etc
+    'email': 'geralt@westeros.com',
     'deployedURLLink': '',//leave this blank for the first exercise
     'apiDocumentationURL': '', //leave this also blank for the first exercise
-    'currentCity': 'The Wall',
-    'hobbies': ['Fight White Walkers']
+    'currentCity': 'Rivia',
+    'hobbies': ['fighting_monsters', 'rewards']
 
-  });
+  })
 });
 /*
  * Get All books information
@@ -81,21 +82,26 @@ app.get('/api/books/', (req, res) => {
  * Add a book information into database
  */
 app.post('/api/books/', (req, res) => {
+
   /*
    * New Book information in req.body
    */
-  console.log(req.body);
+  // console.log(req.body);
   /*
-   * use the books model and create a new object
+   * FIXME: use the books model and create a new object
    * with the information in req.body
    */
-  db.books.create(req.body, (err, newBook) => {
+  /*
+   * return the new book information object as json
+   */
+  var newBook = req.body; // FIXME: ; needs to check later
+
+  db.books.create(newBook, function (err, newBook) {
     if (err) throw err;
-    /*
-     * return the new book information object as json
-     */
+
     res.json(newBook);
-  });
+  })
+  
 });
 
 /*
@@ -107,18 +113,21 @@ app.put('/api/books/:id', (req, res) => {
    */
   const bookId = req.params.id;
   const bookNewData = req.body;
-  console.log(`book ID = ${bookId} \n Book Data = ${bookNewData}`);
+  // console.log(`book ID = ${bookId} \n Book Data = ${bookNewData}`);
+
   /*
-   * use the books model and find using the bookId and update the book information
+   * TODO: use the books model and find using the bookId and update the book information
    */
-  db.books.findOneAndUpdate({_id: bookId}, bookNewData, {new: true},
-                            (err, updatedBookInfo) => {
+  /*
+   * FIXME: Send the updated book information as a JSON object
+   */
+  // var updatedBookInfo = bookNewData;
+
+  db.books.findByIdAndUpdate(bookId, bookNewData, {new:true}, function update(err, updatedBookInfo) {
     if (err) throw err;
-    /*
-     * Send the updated book information as a JSON object
-     */
     res.json(updatedBookInfo);
-  });
+  })
+  
 });
 /*
  * Delete a book based upon the specified ID
@@ -129,22 +138,31 @@ app.delete('/api/books/:id', (req, res) => {
    */
   const bookId = req.params.id;
   /*
-   * use the books model and find using the bookId and delete the book
+   * FIXME: use the books model and find using
+   * the bookId and delete the book
    */
-  db.books.findOneAndRemove({_id: bookId}, (err, deletedBook) => {
+  /*
+   * Send the deleted book information as a JSON object
+   */
+  // var deletedBook = {};
+  db.books.findByIdAndDelete(bookId, (err, deletedBook) => {
     if (err) throw err;
-    /*
-     * Send the deleted book information as a JSON object
-     */
     res.json(deletedBook);
-  });
+  })
+  
 });
-// TODO:  Add API end point /api/exercise2
+
+
+app.get('/api/exercise2', (req, res) => {
+  res.send({"group 21 application deployed using docker"});
+});
+
+
 /**********
  * SERVER *
  **********/
 
 // listen on the port 3000
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Express server is up and running on http://localhost:3000/');
+app.listen(process.env.PORT || 80, () => {
+  console.log('Express server is up and running on http://localhost:80/');
 });
